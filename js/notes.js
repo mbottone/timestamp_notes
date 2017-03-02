5d0e1da0517307779cb57b24ac44939c68a4194f
mbottone <mbottone@squareup.com>
2017-03-02 15:07:59 -0500

    Initial sommit

---
+++ js/notes.js
@@ -0,0 +1,35 @@
+$(function() {
+    $('#notes-form').on('submit', function() {
+    	let noteText = $('#note-text').val();
+    	if (noteText == undefined || noteText == '') {
+    		return false
+    	}
+	    takeNote(noteText);
+	    $('#note-text').val('');
+	    return false;
+	});
+
+	$('#clearBtn').on('click', function() {
+		clearNotes();
+	})
+
+	if (localStorage.notes != undefined) {
+		$('#notes').html(localStorage.notes);
+	}
+});
+
+function takeNote(text) {
+	let timeStamp = getTimeStamp();
+	let note = "<li><b>" + timeStamp + "</b> - " + text + "</li>";
+	$('#notes').append(note);
+	localStorage.notes = $('#notes').html();
+}
+
+function getTimeStamp() {
+    return new Date().toLocaleTimeString();
+}
+
+function clearNotes() {
+	localStorage.notes = '';
+	$('#notes').html('');
+}
